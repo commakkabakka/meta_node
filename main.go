01c6ce9ec0ba00ae7b3d0658c14f720c73ad2a70
Hello <hello@makkabakka.com>
2025-10-08 14:38:59 +0800

    #✍-任务2-语言进阶

--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"meta_node/task1"
-)
+import "meta_node/task2"
 
 func main() {
-	ret1 := task1.SingleNumber([]int{5, 1, 3, 1, 3})
-	fmt.Println(ret1)
+	// ----------------------------------------------------------
+	// task1 test
 
-	ret2 := task1.IsPalindrome(12321)
-	fmt.Println(ret2)
+	//ret1 := task1.SingleNumber([]int{5, 1, 3, 1, 3})
+	//fmt.Println(ret1)
+	//
+	//ret2 := task1.IsPalindrome(12321)
+	//fmt.Println(ret2)
+	//
+	//ret3 := task1.IsValid("({}[({})])")
+	//fmt.Println(ret3)
+	//
+	//ret4 := task1.LongestCommonPrefix([]string{"flower", "flow", "flight"})
+	//fmt.Println(ret4)
+	//
+	//ret5 := task1.PlusOne([]int{9, 9, 9})
+	//fmt.Println(ret5)
+	//
+	//arr := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	//ret6 := task1.RemoveDuplicates(arr)
+	//fmt.Println(ret6, arr)
+	//
+	//ret7 := task1.Merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}})
+	//fmt.Println(ret7)
 
-	ret3 := task1.IsValid("({}[({})])")
-	fmt.Println(ret3)
+	// ----------------------------------------------------------
+	// task2 test
 
-	ret4 := task1.LongestCommonPrefix([]string{"flower", "flow", "flight"})
-	fmt.Println(ret4)
+	task2.Test21()
+	task2.Test22()
+	task2.Test23()
+	task2.Test24()
+	task2.Test25()
+	task2.Test26()
+	task2.Task27()
+	task2.Task28()
+	task2.Task29()
+	task2.Task210()
 
-	ret5 := task1.PlusOne([]int{9, 9, 9})
-	fmt.Println(ret5)
-
-	arr := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	ret6 := task1.RemoveDuplicates(arr)
-	fmt.Println(ret6, arr)
-
-	ret7 := task1.Merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}})
-	fmt.Println(ret7)
-
-	fmt.Println("Hello World.")
+	//fmt.Println("Hello World.")
 }
